f84f8c1b01de4e23642c3f4dd188bad14347b32b
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-11-09 17:16:37 -0800

    Add buttons for adding todo lists and todo items

--- src/view.js
+++ src/view.js
@@ -1,6 +1,25 @@
 // DisplayController factory function here
 export function displayController() {
-  const header = document.createElement('h1');
+  const todoItemsDiv = document.getElementById('todo-items');
+  const todoItemsHeader = document.createElement('p');
+  todoItemsHeader.setAttribute('id', 'list-name');
+  todoItemsHeader.textContent = 'Current Project We Are In'; // this will change depending on what list we're in
+  todoItemsDiv.appendChild(todoItemsHeader);
+
+  const updateScreen = () => {
+    // Write Code Here
+  };
+
+  function clickHandler(e) {
+    // Write Code Here
+  }
+
+  function clearScreen() {
+    // Write Code Here
+  }
+
+  //   updateScreen() // initial render
+
   return {};
 }
 
@@ -8,5 +27,3 @@ export function displayController() {
 // nav bar
 // side bar like admin dashboard
 // then main dashboard
-// we can use css grid for this i believe
-// maybe even flex
