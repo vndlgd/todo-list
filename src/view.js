62d0fe17428f54d38c4acf965efb0b15615ec4d2
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-11-13 23:19:12 -0800

    Implement Menu Item Highlighting in Header

--- src/view.js
+++ src/view.js
@@ -1,61 +1,64 @@
 // DisplayController factory function here
 export function displayController() {
-  // this logic belongs wherever we handle when we click on a new project
-  const todoItemsDiv = document.getElementById('todo-items');
-  const todoItemsHeader = document.createElement('p');
-  todoItemsHeader.setAttribute('id', 'list-name');
-  todoItemsHeader.textContent = 'Current Project We Are In'; // this will change depending on what list we're in
-  todoItemsDiv.appendChild(todoItemsHeader);
-
   // new Date needs to be the user's timezone
   // then fix the length UI issue
   const datePicker = (document.getElementById('due-date').valueAsDate =
     new Date());
 
-  // New Task Modal
-  const addTodoItemButton = document.getElementById('add-task-button');
-  const taskDialog = document.querySelector('#new-task');
-  const taskCloseButton = document.querySelector('#new-task button');
+  // open and close task modal
+  const newTaskModal = (function () {
+    const addTodoItemButton = document.getElementById('add-task-button');
+    const taskDialog = document.querySelector('#new-task');
+    const taskCloseButton = document.querySelector('#new-task button');
 
-  addTodoItemButton.addEventListener('click', () => {
-    taskDialog.showModal();
-  });
+    addTodoItemButton.addEventListener('click', () => {
+      taskDialog.showModal();
+    });
 
-  taskCloseButton.addEventListener('click', () => {
-    taskDialog.close();
-  });
+    taskCloseButton.addEventListener('click', () => {
+      taskDialog.close();
+    });
+  })();
 
-  // New Project Modal
-  const addProjectsButton = document.getElementById('add-project-button');
-  const projectDialog = document.querySelector('#new-project');
-  const projectCloseButton = document.querySelector('#new-project button');
+  // open and close project modal
+  const newProjectModal = (function () {
+    const addProjectsButton = document.getElementById('add-project-button');
+    const projectDialog = document.querySelector('#new-project-dialog');
+    const projectCloseButton = document.querySelector(
+      '#new-project-dialog button'
+    );
 
-  addProjectsButton.addEventListener('click', () => {
-    projectDialog.showModal();
-  });
+    addProjectsButton.addEventListener('click', () => {
+      projectDialog.showModal();
+    });
 
-  projectCloseButton.addEventListener('click', () => {
-    projectDialog.close();
-  });
+    projectCloseButton.addEventListener('click', () => {
+      projectDialog.close();
+    });
+  })();
 
-  const updateScreen = () => {
-    // Write Code Here
-  };
+  // handle updating project header text on click
+  const updateProjectHeader = (function () {
+    const todoItemsDiv = document.getElementById('todo-items');
+    const currentProjectHeader = document.createElement('p');
+    currentProjectHeader.setAttribute('id', 'project-name');
+    const buttons = document.querySelectorAll('.btn');
+    // have 'All Tasks' display as initial value on refresh
+    // TODO: have it handle the projects we create in the submenu as well
+    const initialValue = document.getElementById('initial-value');
+    currentProjectHeader.textContent = initialValue.value;
+    todoItemsDiv.appendChild(currentProjectHeader);
+    buttons.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        currentProjectHeader.textContent = e.target.value;
+        todoItemsDiv.appendChild(currentProjectHeader);
+      });
+    });
+  })();
 
-  function clickHandler(e) {
-    // Write Code Here
-    alert(e.type);
-  }
-
-  function clearScreen() {
-    // Write Code Here
-  }
-
-  //   updateScreen() // initial render
+  // TODO: add new projects to submenu
+  // have a General project always present
 
   return {};
 }
-
-// Think of how you wanna break up each module into functions
-// Organize rather than have it all be here
-// We need to update the project list and todo items list
